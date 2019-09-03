034bf1382c10f9339c07c58264efc50d9295f7e7
Tyler Hardy <tylerthardy@gmail.com>
2019-09-03 00:40:16 -0500

    Working books frontend

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Apollo} from 'apollo-angular';
+import gql from 'graphql-tag';
+import { ApolloQueryResult } from 'apollo-client';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  author: any;
+  loading = true;
+  error: any;
 
-  constructor() {}
+  constructor(private apollo: Apollo) {}
 
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+        query {
+          author(id: 21559){
+            name,
+            books {
+              title
+              isbn,
+              authors {
+                name
+              }
+            }
+          }
+        }
+        `,
+      })
+      .valueChanges.subscribe((result: ApolloQueryResult<any>) => {
+        this.author = result.data && result.data.author;
+        this.loading = result.loading;
+        this.error = result.errors;
+      });
+  }
 }
