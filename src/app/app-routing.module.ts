6060f69daeb18568dc2546b4738d36d1561163e4
Tyler Hardy <tylerthardy@gmail.com>
2019-09-11 23:55:37 -0500

    Route to a default loadout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'loadout/5d7745763eb09a43c4659686', pathMatch: 'full' },
-  { path: 'loadout', redirectTo: 'loadout/5d7745763eb09a43c4659686', pathMatch: 'full' },
+  { path: '', redirectTo: 'loadout/5d784db51a323633d41e2858', pathMatch: 'full' },
+  { path: 'loadout', redirectTo: 'loadout/5d784db51a323633d41e2858', pathMatch: 'full' },
   { path: 'loadout/:loadoutId', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
 ];
 
